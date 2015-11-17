d1f3aad1626659f0c350b006c18121ae472b8536
thiagobustamante <thiago@cruxframework.org>
2015-11-17 17:14:59 -0200

    IGNORE - Reflecting changes on crux core

--- src/main/java/org/cruxframework/crux/widgets/rebind/filter/FilterFactory.java
+++ src/main/java/org/cruxframework/crux/widgets/rebind/filter/FilterFactory.java
@@ -15,10 +15,6 @@
  */
 package org.cruxframework.crux.widgets.rebind.filter;
 
-import org.cruxframework.crux.core.client.utils.EscapeUtils;
-import org.cruxframework.crux.core.rebind.AbstractProxyCreator.SourcePrinter;
-import org.cruxframework.crux.core.rebind.screen.widget.AttributeProcessor;
-import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreator;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreatorContext;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasAllKeyHandlersFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasAnimationFactory;
@@ -26,7 +22,9 @@ import org.cruxframework.crux.core.rebind.screen.widget.creator.HasSelectionHand
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasTextFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasValueChangeHandlersFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.DeclarativeFactory;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.ProcessingTime;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttribute;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttribute.WidgetReference;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttributes;
 import org.cruxframework.crux.gwt.rebind.CompositeFactory;
 import org.cruxframework.crux.widgets.client.filter.Filter;
@@ -48,7 +46,8 @@ import org.cruxframework.crux.widgets.client.filter.Filterable;
 	@TagAttribute(value="popupStyleName", supportsResources=true),
 	@TagAttribute(value="tabIndex", type=Integer.class),
 	@TagAttribute("value"),
-	@TagAttribute(value="filterable", processor=FilterFactory.FilterableAttributeParser.class)
+	@TagAttribute(value="filterable", processingTime=ProcessingTime.afterAllWidgetsOnView, type=WidgetReference.class,
+				 widgetType=Filterable.class)
 })
 
 public class FilterFactory extends CompositeFactory<WidgetCreatorContext> 
@@ -56,35 +55,6 @@ public class FilterFactory extends CompositeFactory<WidgetCreatorContext>
 	              HasValueChangeHandlersFactory<WidgetCreatorContext>, HasSelectionHandlersFactory<WidgetCreatorContext>,
 	              HasAllKeyHandlersFactory<WidgetCreatorContext>
 {
-	/**
-	 * @author Gesse Dafe
-	 */
-	public static class FilterableAttributeParser extends AttributeProcessor<WidgetCreatorContext>
-	{
-		public FilterableAttributeParser(WidgetCreator<?> widgetCreator)
-        {
-	        super(widgetCreator);
-        }
-
-		/**
-		 * @see org.cruxframework.crux.core.rebind.screen.widget.AttributeProcessor#processAttribute(org.cruxframework.crux.core.rebind.screen.widget.ViewFactoryCreator.SourcePrinter, org.cruxframework.crux.core.rebind.screen.widget.WidgetCreatorContext, java.lang.String)
-		 */
-		public void processAttribute(SourcePrinter out, WidgetCreatorContext context, String propertyValue)
-		{
-			String widget = context.getWidget();
-			String filterableId = context.readWidgetProperty("filterable");
-			String filterableWidget = getWidgetCreator().createVariableName("filterable");
-
-			String widgetClassName = getWidgetCreator().getWidgetClassName();
-			printlnPostProcessing("final "+widgetClassName+" "+widget+" = ("+widgetClassName+")"+ getViewVariable()+".getWidget("+EscapeUtils.quote(context.getWidgetId())+");");
-			printlnPostProcessing("Widget "+filterableWidget+" = null;");
-			printlnPostProcessing(filterableWidget+" = "+getViewVariable()+".getWidget("+EscapeUtils.quote(filterableId)+");");
-			printlnPostProcessing("if("+filterableWidget+" != null){");
-			printlnPostProcessing(widget+".setFilterable(("+Filterable.class.getCanonicalName()+"<?>) "+filterableWidget+");");
-			printlnPostProcessing("}");
-		}		
-	}
-	
 	@Override
     public WidgetCreatorContext instantiateContext()
     {
